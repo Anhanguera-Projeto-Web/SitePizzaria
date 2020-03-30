26298c00a4205844ebb1cfbd79e3c6c5be00b101
brianmonteiro54 <brianmonteiro54@yahoo.com.br>
2020-03-30 20:45:09 -0300

    Versão 2 do header e footer

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Trabalho de fim de semestre, uma pizzaria online.
   Nesta branch ficará os arquivos de Produção, onde fica a versão finalizada do projeto, iremos atualizar de tempos em tempos.
   
   Link para teste: http://jujubazeda.rf.gd/pizzaria
-  <BR>Link para Header_footer-v2 https://projetopizza.online/
---
+++ js/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/menu/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/menu/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/menu/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/menu/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/menu/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/menu/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/menu/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
